9920f09fb67ebac187936edfe3cba611d135415f
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-11-24 14:29:55 -0800

    Create post

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::PostsController < ApplicationController
+  def create
+    post = Post.new post_params
+    
+    post.save
+    render json: post, status: :created
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
 end
