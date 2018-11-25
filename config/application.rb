d0fbdf2ba13848a33fa097d681bd7238f79e97e1
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-11-25 10:55:41 -0800

    Sets up Rack CORS

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,17 @@ module BlogReact
       # generator
       g.assets = false
     end
+    config.middleware.insert_before(0, Rack::Cors) do
+      allow do
+        origins "localhost:3001"  
+        resource(
+          "/api/*",
+          headers: :any,
+          methods: [:get, :post, :delete, :patch, :put, :options]
+        )
+      end
+    end
   end
 end
+
+
