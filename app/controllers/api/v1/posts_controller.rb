5fb76684c349e0aa0bbe37322a6f408ea6a6949a
Rosa Diaz Jara <rosadiazjara@gmail.com>
2018-11-25 20:12:31 -0800

    Edits posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -16,6 +16,12 @@ class Api::V1::PostsController < ApplicationController
     render json: post
   end
 
+  def update
+    post = Post.find params[:id]  
+    post.update post_params
+    render json:post, status: :updated
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :content)
